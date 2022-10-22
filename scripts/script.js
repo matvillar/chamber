4a93ad26fd3df675f6e81fb5a967efd8574e8dca
Matias Villar <95631527+matvillar@users.noreply.github.com>
2022-10-22 09:46:32 -0600

    Fixed Several Issues

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,20 @@
 // lastEdit();
 
 const lastUpdate = document.getElementById('date');
+const burger = document.querySelector('.burger-menu');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+document.querySelectorAll('.menu li').forEach(n =>
+  n.addEventListener('click', () => {
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+  })
+);
 
 document.getElementById('year').textContent = new Date().getFullYear();
 function lastEdit() {
