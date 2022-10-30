56645a27fa3b3b28f6a57358a5de812f7c138b20
Matias Villar <95631527+matvillar@users.noreply.github.com>
2022-10-30 12:15:09 -0600

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,42 @@
-// const lastModify = document.getElementById('last-modify');
-
-// document.getElementById('year').textContent = new Date().getFullYear();
-// function lastEdit() {
-//   lastModify.textContent = document.lastModified;
-// }
-
-// lastEdit();
-
+const dateHeader = document.getElementById('dateHeader');
 const lastUpdate = document.getElementById('date');
 const burger = document.querySelector('.burger-menu');
 const menu = document.querySelector('.menu');
 
+// Current Date Header
+let date = new Date();
+
+// Create a list of names for the months and weekdays
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+const weekday = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+// return a formatted date
+dateHeader.innerHTML = `${weekday[date.getDay()]}, ${
+  months[date.getMonth()]
+} ${date.getDate()} ${date.getFullYear()}`;
+
+// Burger Menu
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   menu.classList.toggle('active');
