4d1804b02c3bfe7f32336e8d48a754653d942bd7
Matias <matiase.villar@gmail.com>
2022-11-15 06:21:24 -0700

    Windchill display

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,3 +1,20 @@
 const windSpeed = document.getElementById('windSpeed');
 const windChillSpan = document.getElementById('windChill');
-const tempeture = document.getElementById('temp');
+const temperature = document.getElementById('temp');
+
+const windChillFactor = () => {
+  const speedNum = parseFloat(windSpeed.innerHTML);
+  const tempNum = parseFloat(temperature.innerHTML);
+
+  if (tempNum <= 50 && speedNum >= 3) {
+    const formula =
+      35.74 +
+      0.6215 * tempNum -
+      35.75 * speedNum ** 0.16 +
+      0.4275 * tempNum * speedNum ** 0.16;
+    windChillSpan.innerHTML = formula.toFixed(1);
+  } else {
+    windChillSpan.innerHTML = 'N/A';
+  }
+};
+windChillFactor();
