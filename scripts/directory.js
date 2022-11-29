de21556bab5e6b6656a81f6a3870bc829906b008
Matias <matiase.villar@gmail.com>
2022-11-29 16:49:05 -0500

    updates

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,6 +1,9 @@
 const requestURL = 'https://matvillar.github.io/chamber/json/data.json';
 const directorySection = document.querySelector('.directory-section');
 const directoryCards = document.querySelector('.directory-cards');
+const directoryLists = document.querySelector('.directory-lists');
+const circledMenu = document.getElementById('circledMenu');
+const listMenu = document.getElementById('listMenu');
 
 fetch(requestURL)
   .then((response) => {
@@ -9,6 +12,7 @@ fetch(requestURL)
   .then((jsonObject) => {
     const section = document.createElement('section');
     jsonObject['businessDir'].forEach(displayBusinessCards);
+    jsonObject['businessDir'].forEach(displayListCards);
   });
 
 function displayBusinessCards(businessCard) {
@@ -36,3 +40,39 @@ function displayBusinessCards(businessCard) {
   address.innerHTML = businessCard.address;
   phone.innerHTML = businessCard.phone;
 }
+
+function displayListCards(listCards) {
+  let directoryList = document.createElement('div');
+  let companyName = document.createElement('p');
+  let address = document.createElement('p');
+  let phone = document.createElement('p');
+  let webLink = document.createElement('a');
+  // setting attributes
+  directoryList.setAttribute('class', 'directory-list');
+  webLink.setAttribute('href', listCards.url);
+  // Append Childs
+  directoryList.appendChild(companyName);
+  directoryList.appendChild(address);
+  directoryList.appendChild(phone);
+  directoryList.appendChild(webLink);
+  directoryLists.appendChild(directoryList);
+  directorySection.appendChild(directoryLists);
+  //Inner Html
+  companyName.innerHTML = listCards.name;
+  webLink.innerHTML = listCards.url;
+  address.innerHTML = listCards.address;
+  phone.innerHTML = listCards.phone;
+}
+
+circledMenu.addEventListener('click', () => {
+  directoryLists.classList.add('d-none');
+  directoryCards.classList.remove('d-none');
+  circledMenu.classList.add('active');
+  listMenu.classList.remove('active');
+});
+listMenu.addEventListener('click', () => {
+  directoryCards.classList.add('d-none');
+  directoryLists.classList.remove('d-none');
+  circledMenu.classList.remove('active');
+  listMenu.classList.add('active');
+});
